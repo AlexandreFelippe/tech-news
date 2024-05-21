f998d9f9263f7e7f454ef6a57290c16d759aa180
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-21 16:18:58 -0300

    Resolve requisito 06

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,34 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import ReadingPlanService
+from unittest.mock import Mock, patch
+import pytest
+
+
+fake_news = [
+    {"title": "Notícia 1", "reading_time": 5},
+    {"title": "Notícia 2", "reading_time": 8},
+    {"title": "Notícia 3", "reading_time": 12},
+    {"title": "Notícia 4", "reading_time": 3},
+]
+
+result = {
+    "readable": [
+        {"unfilled_time": 2, "chosen_news": [
+            ("Notícia 1", 5), ("Notícia 4", 3)]},
+        {"unfilled_time": 2, "chosen_news": [("Notícia 2", 8)]}
+    ],
+    "unreadable": [("Notícia 3", 12)]
+}
 
 
 def test_reading_plan_group_news():
-    pass
+    mock = Mock(return_value=fake_news)
+
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(0)
+
+    with patch(
+        "tech_news.analyzer.reading_plan.ReadingPlanService._db_news_proxy",
+        mock
+    ):
+        assert ReadingPlanService.group_news_for_available_time(10) == result
+        mock.assert_called_once()
