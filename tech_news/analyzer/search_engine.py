526ea76fc90819196a06e13c30d92315db34022b
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-21 16:40:06 -0300

    Resolve requisito 08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -12,8 +13,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        formatted_date = datetime.strptime(
+            date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+    query = {"timestamp": formatted_date}
+    news = search_news(query)
+
+    result = [(new["title"], new["url"]) for new in news]
+    return result
 
 
 # Requisito 9
